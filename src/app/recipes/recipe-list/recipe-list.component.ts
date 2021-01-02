8ebbfefacef005e18cbc7fcc4a5c2f23eedd915a
Si Lam <silam@Sis-MacBook-Pro.local>
2021-01-02 07:30:14 -0600

    Update images

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[]  = [
 
-    new Recipe('A Test Recipe', 'This is a test', 'https://www.sandiegoprosthodontics.com/files/2018/09/Recipes-Banner.jpg')
+    new Recipe('Recipe Pizza', 'Recipe for Pizza', 'https://media.istockphoto.com/photos/mediterranean-pizza-picture-id503818102'),
+    new Recipe('Cheese Soup', 'This is a recipe for Cheese Soup', 'https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/Makeover-Potato-Cheese-Soup_exps22253_HWS133216B07_11_2bC_RMS.jpg')
 
   ];
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    // public name: string = "";
+    // public amount: string = "";
+
+    constructor(public name: string, public amount: string)
+    {
+        // this.name = name;
+        // this.amount = amount;
+
+    }
+}
