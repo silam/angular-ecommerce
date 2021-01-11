a4cca427fbf207b4777eab5bb571cc30b1975076
Si Lam <silam@Sis-MacBook-Pro.local>
2021-01-10 20:23:30 -0600

    Create service and inject

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,14 +10,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe('A Soup Recipe', 'This is simply a test', 'https://d1uz88p17r663j.cloudfront.net/resized/2020_06_03T13_18_11_mrs_ImageRecipes_27933x400_448_448.jpg'),
+  //   new Recipe('A Pizza Recipe', 'This is simply a test', 'https://tse1.mm.bing.net/th?id=OIP.klX7K04cCjdzahpDqNOK0AHaHm&pid=Api&rs=1&c=1&qlt=95&w=113&h=116')
+  // ];
 
-  constructor() { }
+  recipes: Recipe[] | undefined;
+
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
